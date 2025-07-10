50d716032f1451cea980786c741d0a92083983b3
mauJS-Py <ramniakogumusipa@outlook.com>
2025-07-10 12:21:56 -0300

    TP Final de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
-import { Products } from './products/products';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Products, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
